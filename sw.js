4dc51e606515aa54f6e2829af2d7c08101636e63
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-24 17:09:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v85-weather-latest-20260924';
+const CACHE = 'iv-planner-v86-papi-pdf-toolbar-20260924';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 const PACKAGE_DB = 'ivplanner-package-manager-v1';
 const PACKAGE_DB_VERSION = 1;
